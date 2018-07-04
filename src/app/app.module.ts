f217f26cc56281cc95dbec909f72dbff2dc7d140
spyspy <spyhopping101@gmail.com>
2018-07-05 07:45:49 +0800

    v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { ManageComponent } from './components/manage/manage.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
--- src/app/components/manage/manage.component.ts
+++ src/app/components/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StocksService } from '../../services/stocks.service';
 
 @Component({
   selector: 'app-manage',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage.component.css']
 })
 export class ManageComponent implements OnInit {
-
-  constructor() { }
+  symbols: Array<string>;                                           
+  stock: string;                                                        
+  constructor(private service: StocksService) {                      
+    this.symbols = service.get();                                   
+  }                                                                
+  add() {                                                         
+    this.symbols = this.service.add(this.stock.toUpperCase());       
+    this.stock = '';                                                 
+  }                                                                
+  remove(symbol) {                                                  
+    this.symbols = this.service.remove(symbol);                     
+  }  
 
   ngOnInit() {
   }
