19ad25a23328cc7364e806e6f923d16a200893a0
spyspy <spyhopping101@gmail.com>
2018-07-15 17:11:46 +0800

    0715

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './components/dashboard/dashboard.component';                          
-import { ManageComponent } from './components/manage/manage.component'; 
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [                                               
-  {                                                                    
-    path: 'dashboard',                                                        
-    component: DashboardComponent                                    
-  },                                                                  
-  {                                                                
-    path: 'manage',                                                   
-    component: ManageComponent                                       
-  }                                                                
-];      
+//Important for Route
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch:'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'manage', component: ManageComponent },
+  { path: '**', component: DashboardComponent },
+
+];
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes,{ enableTracing: true }) //For Debug only!!
+    RouterModule.forRoot(routes, { enableTracing: true }) //For Debug only!!
   ],
-  exports: [ RouterModule ],
-
+  exports: [RouterModule], //Important for Route
   declarations: []
 })
-export class AppRoutingModule { }           
+export class AppRoutingModule { }
 
